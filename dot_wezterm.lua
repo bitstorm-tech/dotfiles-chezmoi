161af5d8952f473e58655a6ea0fc71fccdbc7499
Josef Bauer <bauer.josef1@swm.de>
2024-03-04 13:07:37 +0100

    Change Wezterm color scheme

--- dot_wezterm.lua
+++ dot_wezterm.lua
@@ -1,7 +1,7 @@
 local wezterm = require 'wezterm'
 local config = wezterm.config_builder()
 
-config.color_scheme = 'Darcula'
+config.color_scheme = 'zenbones_dark'
 config.font = wezterm.font('FiraCode Nerd Font', { weight = 'Regular' })
 config.font_size = 19
 
