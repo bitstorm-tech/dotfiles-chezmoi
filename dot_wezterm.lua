28f3c29fe66dd0301534a88e754f9136cc571cc8
Josef Bauer <bauer.josef1@swm.de>
2024-03-04 11:28:58 +0100

    Increase font size for Wezterm

--- dot_wezterm.lua
+++ dot_wezterm.lua
@@ -3,7 +3,7 @@ local config = wezterm.config_builder()
 
 config.color_scheme = 'Darcula'
 config.font = wezterm.font('FiraCode Nerd Font', { weight = 'Regular' })
-config.font_size = 18
+config.font_size = 19
 
 config.hide_tab_bar_if_only_one_tab = true
 config.use_fancy_tab_bar = false
