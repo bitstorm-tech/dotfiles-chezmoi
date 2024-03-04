49dee89f638d6e2e83cd87762fd8d70e0d44bca2
Josef Bauer <bauer.josef1@swm.de>
2024-03-04 12:22:14 +0100

    Add common zshrc file

---
+++ dot_config/zsh/common.zsh
@@ -0,0 +1,20 @@
+eval "$(/opt/homebrew/bin/brew shellenv)"
+eval "$(zoxide init --cmd cd zsh)"
+
+export TERM='xterm-256color'
+export EDITOR='nvim'
+export VISUAL='nvim'
+
+export PATH="/opt/homebrew/opt/coreutils/libexec/gnubin:$PATH"
+export PATH="/opt/homebrew/opt/grep/libexec/gnubin:$PATH"
+
+alias ll='ls -lh --color --group-directories-first'
+alias la='ll -a'
+alias lg='lazygit'
+alias ..='cd ..'
+alias ...='cd ../..'
+alias ....='cd ../../..'
+alias vi='nvim'
+alias dotfiles='chezmoi'
+
+eval "$(starship init zsh)"
